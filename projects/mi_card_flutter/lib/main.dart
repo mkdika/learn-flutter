2c890292a26129f28ea1877b9bcc272bc09b7123
Maikel Chandika <mkdika@gmail.com>
2020-03-06 10:40:19 +0700

    mi_card-flutter - Finished all

--- projects/mi_card_flutter/lib/main.dart
+++ projects/mi_card_flutter/lib/main.dart
@@ -11,34 +11,104 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-//              width: double.infinity,
-//              margin: EdgeInsets.symmetric(vertical: 50, horizontal: 10),
-              color: Colors.red,
-              child: Text('Container 1'),
-            ),
-            SizedBox(height: 10.0), // to give a space between
-            Container(
-              height: 100.0,
-//              width: double.infinity,
-              color: Colors.green,
-              child: Text('Container 2'),
-            ),
-            SizedBox(height: 10.0),
-            Container(
-              height: 100.0,
-//              width: double.infinity,
-              color: Colors.blue,
-              child: Text('Container 3'),
-            ),
-          ],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 80.0,
+                    backgroundColor: Colors.red,
+                    backgroundImage: AssetImage('images/mike.jpg'),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 28.0,
+              ),
+              Text(
+                'Maikel Chandika',
+                style: TextStyle(
+                    fontSize: 32.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+              Text(
+                'BACKEND ENGINEER',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.teal.shade900,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 40.0,
+                ),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                  ),
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Center(
+                    child: Text(
+                      '+62812 6002 2570',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 40.0,
+                ),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 30.0,
+                  ),
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Center(
+                    child: Text(
+                      'mkdika@gmail.com',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+//Row(
+//children: <Widget>[
+//
+//SizedBox(
+//width: 10.0,
+//),
+//],
+//),
